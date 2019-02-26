b2fbba72e3b6abd27aaab252779b98410f4da6c7
julio <juliodisouza@gmail.com>
2019-02-26 13:03:40 -0300

    swiss - aulas: Navigation e Modal

--- App2_TipoPaginaXF/App2_TipoPaginaXF/TipoPagina/Carousel/TipoPagina3.xaml.cs
+++ App2_TipoPaginaXF/App2_TipoPaginaXF/TipoPagina/Carousel/TipoPagina3.xaml.cs
@@ -9,12 +9,18 @@ using Xamarin.Forms.Xaml;
 
 namespace App2_TipoPaginaXF.TipoPagina.Carousel
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class TipoPagina3 : ContentPage
-	{
-		public TipoPagina3 ()
-		{
-			InitializeComponent ();
-		}
-	}
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TipoPagina3 : ContentPage
+    {
+        public TipoPagina3()
+        {
+            InitializeComponent();
+        }
+
+        protected void MudarPagina (object sender, EventArgs args)
+        {
+            //App.Current.MainPage = new NavigationPage(new Navigation.Pagina1()) { BarBackgroundColor = Color.Blue };
+            App.Current.MainPage = new Tabbed.Abas();
+        }
+    }
 }
---
+++ App2_TipoPaginaXF/App2_TipoPaginaXF/TipoPagina/Navigation/Pagina1.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App2_TipoPaginaXF.TipoPagina.Navigation
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Pagina1 : ContentPage
+	{
+		public Pagina1 ()
+		{
+			InitializeComponent ();
+		}
+
+        protected void MudarParaPagina2(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new Pagina2());
+        }
+
+        protected void ChamarModal(object sender, EventArgs args)
+        {
+            Navigation.PushModalAsync(new Navigation.Modal());
+        }
+
+    }
+}
